3ec3793053cb4db6cc7b0afe36ff9b3a277da0be
BlarQ <collinsogunlala@gmail.com>
2024-10-21 19:26:07 +0100

    feat: added favicon, changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Dev-Tective",
+  title: "GitHub Profile Viewer",
   description: "GitHub Developer Profile Detective",
 };
 
